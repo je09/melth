228ea4ef520920e28ffbeb855b9760058f3237e5
Petr <je09@yandex.ru>
2020-05-30 00:04:08 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 sudo docker rmi melth_image --force;
-sudo docker build -t melth .;
+sudo docker build -t melth_image .;
 sudo docker rm melth --force;
 sudo docker run --name melth -p 8003:8003 --restart "always" -d melth_image;
